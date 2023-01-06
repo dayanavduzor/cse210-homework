7f5820f7dc50047fdce964f1293d1be44246bd81
yourusername <van22029@byui.edu>
2023-01-06 10:43:05 -0500

    conditionals letter grades prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Write a program that determines the letter grade for a course according to the following scale:
+        /*
+        A >= 90
+        B >= 80
+        C >= 70
+        D >= 60
+        F < 60
+        */
+        string letter = "";
+
+        Console.Write("What is your grade percentage? ");
+        string percentageInput = Console.ReadLine();
+        int percentage = int.Parse(percentageInput);
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >=80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        // congratulations/ encourage statement:
+        if (percentage >= 70)
+        {
+            Console.WriteLine($"Congraturlations you passed!. Your grade is {letter}");
+        }
+        else 
+        {
+            Console.WriteLine($"Keep trying to pass next time, you can do it! Your grade is {letter}");
+        }
+
+    
     }
 }
