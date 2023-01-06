669b807093cb408b31ec5beab67f85eb7bf66c2f
yourusername <van22029@byui.edu>
2023-01-06 12:29:38 -0500

    letter grade prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,13 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //Write a program that determines the letter grade for a course according to the following scale:
-        /*
-        A >= 90
-        B >= 80
-        C >= 70
-        D >= 60
-        F < 60
-        */
+        
         string letter = "";
 
         Console.Write("What is your grade percentage? ");
