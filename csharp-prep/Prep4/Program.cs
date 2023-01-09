9eaac9500da272e3bb66c4698ab5a03c1d6dca3f
yourusername <van22029@byui.edu>
2023-01-09 12:40:08 -0500

    list prep activity

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Hello Prep 4, Lists");
+
+        List<int> numbers = new List<int>();
+        int number = -1;
+        int total = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string numberInput = Console.ReadLine();
+            number = int.Parse(numberInput);
+            numbers.Add(number);
+        }
+        //lenght of list : 
+        //Console.WriteLine(numbers.Count);
+
+        //to add numbers in list: 
+        foreach (int num in numbers)
+        {
+            total += num;  
+        }
+        Console.WriteLine($"The sum is: {total}");
+        
+        // to find average
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // to find maximum number:
+        int maxNum = numbers[0];
+        
+        foreach (int num in numbers)
+        {
+            if (num > maxNum)
+            {
+                // if new number greater than maxNum, maxNum found. 
+                maxNum = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {maxNum}"); 
     }
 }
